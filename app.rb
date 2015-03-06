180ac7737a594df97d0a649eb7766d680c49cc94
Emily Grevel <grevelem@msu.edu>
2015-03-05 23:25:38 -0500

    further space changes

--- app.rb
+++ app.rb
@@ -3,11 +3,11 @@
   require 'holidapi'
 
   class HolidayApp < Sinatra::Base
-	  get '/' do
-	  	params['month'] ||= 3
-	  	params['year'] ||= 2015
-	  	params['country'] ||="US"
-	 	@holiday = HolidApi.get(month: params['month'], year: params['year'], country: params['country'])
-		erb :index
-	  end
+    get '/' do
+      params['month'] ||= 3
+      params['year'] ||= 2015
+      params['country'] ||="US"
+      @holiday = HolidApi.get(month: params['month'], year: params['year'], country: params['country'])
+    erb :index
+    end
   end
