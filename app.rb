31f6435f570e530bb9b1166f1bc81a7463bc0876
Emily Grevel <grevelem@msu.edu>
2015-03-05 23:50:15 -0500

    indentation fix

--- app.rb
+++ app.rb
@@ -1,13 +1,13 @@
 # app.rb
-  require 'sinatra'
-  require 'holidapi'
+require 'sinatra'
+require 'holidapi'
 
-  class HolidayApp < Sinatra::Base
-    get '/' do
-      params['month'] ||= 3
-      params['year'] ||= 2015
-      params['country'] ||="US"
-      @holiday = HolidApi.get(month: params['month'], year: params['year'], country: params['country'])
+class HolidayApp < Sinatra::Base
+  get '/' do
+    params['month'] ||= 3
+    params['year'] ||= 2015
+    params['country'] ||="US"
+    @holiday = HolidApi.get(month: params['month'], year: params['year'], country: params['country'])
     erb :index
-    end
   end
+end
