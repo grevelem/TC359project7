91f06d3756aac095edecb2794651041da1303ac0
Emily Grevel <grevelem@msu.edu>
2015-03-05 23:06:38 -0500

    spacing fix

--- app.rb
+++ app.rb
@@ -4,10 +4,10 @@
 
   class HolidayApp < Sinatra::Base
 	  get '/' do
-	  		params['month'] ||= 3
-	  		params['year'] ||= 2015
-	  		params['country'] ||="US"
-	 		@holiday = HolidApi.get(month: params['month'], year: params['year'], country: params['country'])
-		 erb :index
+	  	params['month'] ||= 3
+	  	params['year'] ||= 2015
+	  	params['country'] ||="US"
+	 	@holiday = HolidApi.get(month: params['month'], year: params['year'], country: params['country'])
+		erb :index
 	  end
   end
